ea3d5fc51baf46efe9107c574f92adf327f80346
Ryan-Phillips Iserhills Cornelio <ryan.cornelio@gmail.com>
2022-03-23 12:08:48 +0100

    Fixed error message showing on bio...

--- script.js
+++ script.js
@@ -36,48 +36,47 @@ window.onclick = function(event) {
   }
 }
   /* Denna kanske inte behövs */
-/* import html2canvas from 'html2canvas' */
+/*   import html2canvas from 'html2canvas' */
 
-function screenshot(){
-html2canvas(document.querySelector("#capture")).then(canvas => {
-  document.modal.appendChild(canvas)
-});
-}
-/* html2canvas(element, options);
-
-html2canvas(document.body).then(function(canvas) {
-  document.body.appendChild(canvas);
+/*   function screenshot(){
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+      document.modal.appendChild(canvas)
+    });
+  }
+  html2canvas(element, options);
+  
+  html2canvas(document.body).then(function(canvas) {
+    document.body.appendChild(canvas);
 }); */
 
-
 const submit = document.querySelector('#btnConfirm');
-
 
 submit.addEventListener('click', (e) => {
   e.preventDefault()
   let messages = []
-  const bio = document.getElementById('bio').value;
-  console.log(bio);
-  const phone = document.getElementById('phone').value;
-  console.log(phone);
-  const email = document.getElementById('email').value;
-  console.log(email);
+  const bio = document.getElementById('bio');
+
+  const phone = document.getElementById('phone');
+
+  const email = document.getElementById('email');
+
   const errorElement = document.getElementById('error');
 
+  console.log(bio.value)
   if(bio.value === '' || bio.value == null) {
-    messages.push('Bio is required')
-  }
+    messages.push('Please fill in your issue')
+  } 
   
   if (phone.length <= 9) {
-    messages.push('Please enter 10 numbers...')
+    messages.push('Please enter a phone number')
   }
   if (phone.length >= 20) {
-    messages.push('Too many characters...')
+    messages.push('Too many characters')
   }
 
   if (messages.length > 0) {
     e.preventDefault()
-    errorElement.innerText = messages.join(', ')
+    errorElement.innerText = messages.join(' & ')
   }
 
   console.log(e)
