13b0df5b7519452c02db425a26525fa65ead3723
Ryan-Phillips Iserhills Cornelio <ryan.cornelio@gmail.com>
2022-04-08 09:41:23 +0200

    fixed cookie function...

--- script.js
+++ script.js
@@ -156,8 +156,10 @@ const cookieButton = document.querySelector(".cookie-btn");
 
 cookieButton.addEventListener("click", () => {
   cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
 });
 
 setTimeout(() => {
+  if(!localStorage.getItem("cookieBannerDisplayed"))
   cookieContainer.classList.add("active");
 }, 2000);
